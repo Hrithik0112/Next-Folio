3eac80eb44ca7317cb572fb871a06fa52ea861d3
Hrithik <slumdog4321@gmail.com>
2023-08-16 18:20:20 +0530

    Blog Page Created

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,7 +1,61 @@
 import React from "react";
+import styles from "./page.module.css";
+import Image from "next/image";
+import { notFound } from "next/navigation";
 
-const BlogId = () => {
-  return <div>BlogId</div>;
+async function getData(id) {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    return notFound();
+  }
+
+  return res.json();
+}
+
+export async function generateMetadata({ params }) {
+  const post = await getData(params.id);
+  return {
+    title: post.title,
+    description: post.desc,
+  };
+}
+
+const BlogPost = async ({ params }) => {
+  const data = await getData(params.id);
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>{data.title}</h1>
+          <p className={styles.body}>{data.body}</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>{data.username}</span>
+          </div>
+        </div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+            alt=""
+            fill={true}
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.text}>{data.content}</p>
+      </div>
+    </div>
+  );
 };
 
-export default BlogId;
+export default BlogPost;
